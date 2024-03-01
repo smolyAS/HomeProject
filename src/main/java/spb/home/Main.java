3c91b32ef0ce20102c69fd9d48ca44d93b1e6175
smolyAS <smoljakov@mail.ru>
2024-03-01 18:07:50 +0300

    Commit 1.2

--- src/main/java/spb/home/Main.java
+++ src/main/java/spb/home/Main.java
@@ -2,6 +2,10 @@ package spb.home;
 
 public class Main {
     public static void main(String[] args) {
+        Plant.oilProductionNullDistillation();
+        Plant.oilProductionFirstDistillation();
+        Plant.oilProductionSecondDistillation();
+        Plant.oilProductionTreeDistillation();
 
     }
 }
